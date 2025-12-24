35a0ae44f5b81bb8791721730d9060ad7ef4a626
StefanRuseski <ruseskis@gmail.com>
2025-12-24 21:56:37 +0100

    Homework 9 Functions2

--- Homework 9 Functions2/functions2.js
+++ Homework 9 Functions2/functions2.js
@@ -4,3 +4,10 @@ function humanDogAge(humanYears) {
     return dogYears;
 }
 humanDogAge(4); // Should print: "Your rottweiler is 28 years old in dog years."
+
+function dogHumanAge(dogYears) {
+    let humanYears = dogYears / 7;
+    console.log("Your rottweiler is " + humanYears + " years old in human years.");
+    return humanYears;
+}
+dogHumanAge(21); // Should print: "Your rottweiler is 3 years old in human years."
